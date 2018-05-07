22bfefb40e4e60bd55471e2e23eac7b3ff436464
Vikimanish <vikimanish@gmail.com>
2018-05-08 00:48:54 +0530

    last

--- js/custom.js
+++ js/custom.js
@@ -98,8 +98,11 @@ var customScripts = {
 }
 $('document').ready(function () {
 	 $.backstretch([
-      "images/img1.jpg"
-    , "images/img2.jpg"
+      "images/img3.jpg"
+    , "images/img4.jpg",
+    , "images/img5.jpg",
+    , "images/img6.jpg",
+    , "images/img7.jpg",
   ], {duration: 3000, fade: 1250});
   
     customScripts.init();
